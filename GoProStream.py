5ed27f2056aa9d8af77c131a8c013abe57c938b5
konradit <github@chernowii.com>
2017-02-04 10:10:46 +0100

    fixed indentation - tested w/ HERO4

--- GoProStream.py
+++ GoProStream.py
@@ -32,7 +32,7 @@ RECORD=False
 ##
 ## Saves the feed to a custom location
 SAVE=False
-SAVE_FILENAME="goprofeed2"
+SAVE_FILENAME="goprofeed3"
 SAVE_FORMAT="mp4"
 SAVE_LOCATION="/home/konrad/Videos/"
 
@@ -45,60 +45,60 @@ def gopro_live():
 	MESSAGE = get_command_msg(KEEP_ALIVE_CMD)
 	URL = "http://10.5.5.9:8080/live/amba.m3u8"
 	response = urllib.request.urlopen('http://10.5.5.9/gp/gpControl/info').read()
-		if b"HD4" in response or b"HD3.2" in response or b"HD5" in response or b"HX" in response:
-			print("HERO4/HERO5/HERO+ camera")
-			##
-			## HTTP GETs the URL that tells the GoPro to start streaming.
-			##
-			urllib.request.urlopen("http://10.5.5.9/gp/gpControl/execute?p1=gpStream&a1=proto_v2&c1=restart").read()
-			if RECORD:
-				urllib.request.urlopen("http://10.5.5.9/gp/gpControl/command/shutter?p=1").read()
-			print("UDP target IP:", UDP_IP)
-			print("UDP target port:", UDP_PORT)
-			print("message:", MESSAGE)
-			print("Recording on camera: " + str(RECORD))
+	if b"HD4" in response or b"HD3.2" in response or b"HD5" in response or b"HX" in response:
+		print("HERO4/HERO5/HERO+ camera")
+		##
+		## HTTP GETs the URL that tells the GoPro to start streaming.
+		##
+		urllib.request.urlopen("http://10.5.5.9/gp/gpControl/execute?p1=gpStream&a1=proto_v2&c1=restart").read()
+		if RECORD:
+			urllib.request.urlopen("http://10.5.5.9/gp/gpControl/command/shutter?p=1").read()
+		print("UDP target IP:", UDP_IP)
+		print("UDP target port:", UDP_PORT)
+		print("message:", MESSAGE)
+		print("Recording on camera: " + str(RECORD))
 
-			## GoPro HERO4 Session needs status 31 to be greater or equal than 1 in order to start the live feed.
-			if b"HX" in response:
-				connectedStatus=False
-				while connectedStatus == False:
-					req=urllib.request.urlopen("http://10.5.5.9/gp/gpControl/status")
-					data = req.read()
-					encoding = req.info().get_content_charset('utf-8')
-					json_data = json.loads(data.decode(encoding))
-					if json_data["status"]["31"] >= 1:
-						connectedStatus=True
-			##
-			## Opens the stream over udp in ffplay. This is a known working configuration by Reddit user hoppjerka:
-			## https://www.reddit.com/r/gopro/comments/2md8hm/how_to_livestream_from_a_gopro_hero4/cr1b193
-			##
-			loglevel_verbose=""
-			if VERBOSE==False:
-				loglevel_verbose = "-loglevel panic"
-			if SAVE == False:
-				subprocess.Popen("ffplay " + loglevel_verbose + " -fflags nobuffer -f:v mpegts -probesize 8192 udp://:8554", shell=True)
-			else:
-				print("Recording locally: " + str(SAVE))
-				print("Recording stored in: " + SAVE_LOCATION + SAVE_FILENAME + "." + SAVE_FORMAT)
-				print("Note: Preview is not available when saving the stream.")
-				SAVELOCATION=SAVE_LOCATION + SAVE_FILENAME + "." + SAVE_FORMAT
-				subprocess.Popen("ffmpeg -i 'udp://:8554' -fflags nobuffer -f:v mpegts -probesize 8192 " + SAVELOCATION, shell=True)
-			if sys.version_info.major >= 3:
-				MESSAGE = bytes(MESSAGE, "utf-8")
-			print("Press ctrl+C to quit this application.\n")
-			while True:
-				sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-				sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-				sleep(KEEP_ALIVE_PERIOD/1000)
+		## GoPro HERO4 Session needs status 31 to be greater or equal than 1 in order to start the live feed.
+		if b"HX" in response:
+			connectedStatus=False
+			while connectedStatus == False:
+				req=urllib.request.urlopen("http://10.5.5.9/gp/gpControl/status")
+				data = req.read()
+				encoding = req.info().get_content_charset('utf-8')
+				json_data = json.loads(data.decode(encoding))
+				if json_data["status"]["31"] >= 1:
+					connectedStatus=True
+		##
+		## Opens the stream over udp in ffplay. This is a known working configuration by Reddit user hoppjerka:
+		## https://www.reddit.com/r/gopro/comments/2md8hm/how_to_livestream_from_a_gopro_hero4/cr1b193
+		##
+		loglevel_verbose=""
+		if VERBOSE==False:
+			loglevel_verbose = "-loglevel panic"
+		if SAVE == False:
+			subprocess.Popen("ffplay " + loglevel_verbose + " -fflags nobuffer -f:v mpegts -probesize 8192 udp://:8554", shell=True)
 		else:
-			response = urllib.request.urlopen('http://10.5.5.9/camera/cv').read()
-			if b"Hero3" in response:
-				PASSWORD=urllib.request.urlopen("http://10.5.5.9/bacpac/sd").read()
-				print("HERO3/3+/2 camera")
-				Password =  str(PASSWORD, 'utf-8')
-				text=re.sub(r'\W+', '', Password)
-				urllib.request.urlopen("http://10.5.5.9/camera/PV?t=" + text + "&p=%02")
-				subprocess.Popen("ffplay " + URL, shell=True)
+			print("Recording locally: " + str(SAVE))
+			print("Recording stored in: " + SAVE_LOCATION + SAVE_FILENAME + "." + SAVE_FORMAT)
+			print("Note: Preview is not available when saving the stream.")
+			SAVELOCATION=SAVE_LOCATION + SAVE_FILENAME + "." + SAVE_FORMAT
+			subprocess.Popen("ffmpeg -i 'udp://:8554' -fflags nobuffer -f:v mpegts -probesize 8192 " + SAVELOCATION, shell=True)
+		if sys.version_info.major >= 3:
+			MESSAGE = bytes(MESSAGE, "utf-8")
+		print("Press ctrl+C to quit this application.\n")
+		while True:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+			sleep(KEEP_ALIVE_PERIOD/1000)
+	else:
+		response = urllib.request.urlopen('http://10.5.5.9/camera/cv').read()
+		if b"Hero3" in response:
+			PASSWORD=urllib.request.urlopen("http://10.5.5.9/bacpac/sd").read()
+			print("HERO3/3+/2 camera")
+			Password =  str(PASSWORD, 'utf-8')
+			text=re.sub(r'\W+', '', Password)
+			urllib.request.urlopen("http://10.5.5.9/camera/PV?t=" + text + "&p=%02")
+			subprocess.Popen("ffplay " + URL, shell=True)
 		
 
 
